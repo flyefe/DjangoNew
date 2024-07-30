053dd7dd323385844e342035bdef00296e99800a
efe <victorefe5@gmail.com>
2024-07-30 16:32:17 +0100

    team added as dropdown to form

--- maestrocrm/client/forms.py
+++ maestrocrm/client/forms.py
@@ -7,6 +7,6 @@ class AddClientForm(forms.ModelForm):
         fields = [
             'first_name', 'middle_name', 'last_name', 'phone_number', 'email',
             'services', 'status', 'open_date', 'close_date', 'assigned_to',
-            'traffic_source',
+            'traffic_source','team',
         ]
         # Remove readonly_fields if it's not needed

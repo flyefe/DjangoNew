ab63a916cd5e5613ab1ad6f93f665f7e6bdbc9f1
efe <victorefe5@gmail.com>
2024-08-01 21:48:48 +0100

    created plan model without foreign key in team model

--- maestrocrm/team/models.py
+++ maestrocrm/team/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Plan(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+    max_lead = models.IntegerField()
+    max_client = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 class Team(models.Model):
     name = models.CharField(max_length=100)
-    created_by = models.ForeignKey(User, related_name='created_teams', on_delete=models.CASCADE)
     members = models.ManyToManyField(User, related_name='teams') 
+    created_by = models.ForeignKey(User, related_name='created_teams', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
 
     def __str__(self):
         return self.name

97c4095812b0c33fa9776c882f761a6a5771eb91
efe <victorefe5@gmail.com>
2024-08-23 19:34:57 +0100

    signup/logins forms beautified

--- maestrocrm/maestrocrm/urls.py
+++ maestrocrm/maestrocrm/urls.py
@@ -5,9 +5,10 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from core.views import index, about
+from userprofile.forms import LoginForm, SignupForm
 
 urlpatterns = [
-    path('log-in/', auth_views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('log-in/', auth_views.LoginView.as_view(template_name='userprofile/login.html', authentication_form=LoginForm), name='login'),
     # path('lead/', include('lead.urls')),
     path('about/', about, name='about'),
     path('dashboard/client/', include('client.urls')),
